e51c0cb8c7f02e87a4dced9c60e52a553808f93a
PierreFrs <pierrefraisse90@gmail.com>
2023-09-20 15:16:39 +0200

    feat(home-card): Home-card creation
    Home-cards (global desc of UML & merise) created and displayed
    dynamicaly on homepage

--- catagram_frontend/src/app/pages/homepage/homepage.component.ts
+++ catagram_frontend/src/app/pages/homepage/homepage.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as globalDesc from 'src/app/data/global-desc.json';
 
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css']
 })
-export class HomepageComponent {
+export class HomepageComponent implements OnInit {
+  globalDescriptions: any = (globalDesc as any).default;
 
+  ngOnInit(): void {}
 }
