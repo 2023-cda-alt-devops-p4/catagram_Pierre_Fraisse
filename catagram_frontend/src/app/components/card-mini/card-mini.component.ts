28aa7c56f9a412b9364d9e71805b12365ad01e0b
PierreFrs <pierrefraisse90@gmail.com>
2023-09-21 15:40:11 +0200

    feat(UML Page): Display of uml diagrams
    All UML diagrams are dynamicaly displayed as minified cards in my UML
    Page

--- catagram_frontend/src/app/components/card-mini/card-mini.component.ts
+++ catagram_frontend/src/app/components/card-mini/card-mini.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-card-mini',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./card-mini.component.css']
 })
 export class CardMiniComponent {
-
+  @Input() diagram: any;
 }
