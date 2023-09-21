5c4ad24fdd62825ab066c2e10521522ef569f364
PierreFrs <pierrefraisse90@gmail.com>
2023-09-21 15:41:57 +0200

    feat(Merise Page): Display of merise diagrams
    All MERISE diagrams are dynamicaly displayed as minified cards in my
    MERISE
    Page

--- catagram_frontend/src/app/pages/merise/merise.component.ts
+++ catagram_frontend/src/app/pages/merise/merise.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import diagramsData from '../../data/diagrams.json'
 @Component({
   selector: 'app-merise',
   templateUrl: './merise.component.html',
   styleUrls: ['./merise.component.css']
 })
 export class MeriseComponent {
-
+  meriseDiagrams: any = diagramsData[1].diagrams;
 }
