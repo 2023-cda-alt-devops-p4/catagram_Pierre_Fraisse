75f18becdb028a5f00d30c164703492c4d915d84
PierreFrs <pierrefraisse90@gmail.com>
2023-09-21 15:36:23 +0200

    feat(UML Page): Display of uml diagrams
    All UML diagrams are dynamicaly displayed as cards in my UML Page

--- catagram_frontend/src/app/pages/uml/uml.component.ts
+++ catagram_frontend/src/app/pages/uml/uml.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-
+import diagramsData from '../../data/diagrams.json'
 @Component({
   selector: 'app-uml',
   templateUrl: './uml.component.html',
   styleUrls: ['./uml.component.css']
 })
 export class UmlComponent {
-
+  umlDiagrams: any = diagramsData[0].diagrams;
 }
