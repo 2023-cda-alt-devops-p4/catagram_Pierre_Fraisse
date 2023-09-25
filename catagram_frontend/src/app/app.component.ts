f1f60a08c63e12f56255228997f364648f59b2e0
PierreFrs <pierrefraisse90@gmail.com>
2023-09-25 15:45:52 +0200

    feat(navigation): navigation menu display on mobile
    hamburger menu display in the header's hamburger menu when in mobile
    size

--- catagram_frontend/src/app/app.component.ts
+++ catagram_frontend/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NavigationService} from "./services/navigation.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'catagram_frontend';
+  showNavigation = false;
+
+  constructor(private navigationService: NavigationService) {
+  }
+
+  ngOnInit(): void {
+    this.navigationService.navigationVisible$.subscribe(
+      isVisible => this.showNavigation = isVisible
+    );
+  }
 }
--- catagram_frontend/src/app/components/header/header.component.ts
+++ catagram_frontend/src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import { NavigationService } from "../../services/navigation.service";
 
 @Component({
   selector: 'app-header',
@@ -8,7 +9,7 @@ import {Component, OnInit} from '@angular/core';
 export class HeaderComponent implements OnInit {
   isMobile: boolean;
 
-  constructor() {
+  constructor(private navigationService: NavigationService) {
     this.isMobile = window.innerWidth < 768;
   }
 
@@ -16,6 +17,10 @@ export class HeaderComponent implements OnInit {
     window.addEventListener('resize', () => {
       this.isMobile = window.innerWidth < 768; // Again, adjust 768 to your mobile breakpoint
     });
+  }
+
+  toggleNavigation() {
+    this.navigationService.toggleNavigation();
   }
 }
 
